33e70dc99ba85d1998a8321136f60f1585ea6e85
David Márquez Mínguez <45391647+Marquez-David@users.noreply.github.com>
2021-04-26 23:39:16 +0200

    Ultimas mejoras
    Queda la memoria y el envio.

--- PECL2/menu.pl
+++ PECL2/menu.pl
@@ -1,8 +1,9 @@
 % Autores: David Márquez Mínguez y Robert Petrisor
 % Fecha: 27/04/2021
 
-:-consult(gramatica).
-:-consult(draw).
+:-['gramatica.pl'].
+:-['draw.pl'].
+?- set_prolog_flag(character_escapes,false).
 
 analizar:-
   FRASES = [
@@ -20,6 +21,13 @@ analizar:-
   [el,procesador,de,textos,es,una,herramienta,muy,potente,que,sirve,para,escribir,documentos,pero,es,bastante,lento],
   [el,ratón,que,cazó,el,gato,era,gris],
   [el,hombre,que,vimos,ayer,era,mi,vecino]],
+
+  writeln('   _____                .__  .__                    .___           ._.'),
+  writeln('  /  _  \   ____ _____  |  | |__|____________     __| _/___________| |'),
+  writeln(' /  /_\  \ /    \\__  \ |  | |  \___   /\__  \   / __ |/  _ \_  __ \ |'),
+  writeln('/    |    \   |  \/ __ \|  |_|  |/    /  / __ \_/ /_/ (  <_> )  | \/\|'),
+  writeln('\____|__  /___|  (____  /____/__/_____ \(____  /\____ |\____/|__|   __'),
+  writeln('        \/     \/     \/              \/     \/      \/             \/'),
   writeln('Pulsa 1. si desea analizar las oraciones predeterminadas'),
   writeln('Pulsa 2. si desea analizar su propia oracion'),
   writeln('Pulsa 3. si desea simplificar una oracion'),
@@ -28,7 +36,7 @@ analizar:-
 aux(Opcion,FRASES):-
   Opcion = 1, menu(FRASES);
   Opcion = 2, menu2;
-  Opcion = 3, writeln('Introduce la oracion a simplificar'), read(X), simplificar(X,[]);
+  Opcion = 3, writeln('Introduce la oracion a simplificar(ej, [el,hombre,es,guapo,y,el,es,moreno].): '), read(X), simplificar(X,[]);
   writeln('Debes selecciona runa de las tres opciones'), analizar.
 
 menu([]):- writeln('Las frases han acabado').
@@ -40,7 +48,7 @@ menu([H|T]):-
   writeln('Adios!').
   
 menu2:-
-  write('Escribe la frase que desee analizar'), nl,
+  write('Escribe la frase que desee analizar(ej, [el,hombre,que,vimos,ayer,era,mi,vecino].): '), nl,
   write('Escribe 0. para salir del analizador'), nl,
   read(X),
   X \= 0 -> preprocesamiento(X), menu2;
